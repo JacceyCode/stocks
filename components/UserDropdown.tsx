b34e598c9a7260c763d28fc1314c9d0156470500
JACOB ADEBAYO <jacobadebayo.ja@gmail.com>
2025-12-04 21:55:27 +0100

    Updated dropdown Initails

--- components/UserDropdown.tsx
+++ components/UserDropdown.tsx
@@ -57,9 +57,12 @@ const UserDropdown = ({
         <DropdownMenuLabel>
           <div className="flex relative items-center gap-3 py-2">
             <Avatar className="h-10 w-10">
-              <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" />
+              {/* <AvatarImage src="https://avatars.githubusercontent.com/u/153423955?s=280&v=4" /> */}
               <AvatarFallback className="bg-yellow-500 text-yellow-900 text-sm font-bold">
-                {user.name[0]}
+                {user.name
+                  .split(" ")
+                  .map((word) => word[0])
+                  .join("")}
               </AvatarFallback>
             </Avatar>
 
